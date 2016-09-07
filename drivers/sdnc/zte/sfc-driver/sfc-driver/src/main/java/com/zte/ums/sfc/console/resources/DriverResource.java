5abc2038a8e67fd697becf121b49cb39b7172887
fujinhua <fu.jinhua@zte.com.cn>
2016-09-07 15:03:59 +0800

    Solve checkstyle problems
    Change-Id: I5a827aee75cad5b2058ebe2a4a8897c468b59e8c
    Signed-off-by: fujinhua <fu.jinhua@zte.com.cn>

--- drivers/sdnc/zte/sfc-driver/sfc-driver/src/main/java/com/zte/ums/sfc/console/resources/DriverResource.java
+++ drivers/sdnc/zte/sfc-driver/sfc-driver/src/main/java/com/zte/ums/sfc/console/resources/DriverResource.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright 2016 [ZTE] and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.zte.ums.sfc.console.resources;
 
 import com.codahale.metrics.annotation.Timed;
@@ -13,13 +28,7 @@ import org.slf4j.LoggerFactory;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 
-/**
- * Created with IntelliJ IDEA.
- * User: 10084662
- * Date: 16-9-1
- * Time: ÏÂÎç7:16
- * To change this template use File | Settings | File Templates.
- */
+
 @Path("/")
 public class DriverResource {
     private static final Logger LOGGER = LoggerFactory.getLogger(DriverResource.class);
