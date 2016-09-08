890bf8d076adb86015ad9c8f17d7bb36e5125d17
fujinhua <fu.jinhua@zte.com.cn>
2016-09-08 09:14:20 +0800

    Solve checkstyle problems
    Change-Id: I341b9407a8dec134c89a6522e4e42a553418a163
    Signed-off-by: fujinhua <fu.jinhua@zte.com.cn>

--- drivers/sdnc/zte/sfc-driver/plugin-standalone/src/main/assembly/run.bat
+++ drivers/sdnc/zte/sfc-driver/plugin-standalone/src/main/assembly/run.bat
@@ -1,5 +1,5 @@
 @REM
-@REM   Copyright (C) 2016 ZTE, Inc. and others. All rights reserved. (ZTE)
+@REM   Copyright 2016 [ZTE] and others.
 @REM
 @REM Licensed under the Apache License, Version 2.0 (the "License");
 @REM you may not use this file except in compliance with the License.
