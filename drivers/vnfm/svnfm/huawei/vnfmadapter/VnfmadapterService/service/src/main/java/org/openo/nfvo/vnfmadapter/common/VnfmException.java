bd22ef4d40737a27cb79644fe81f0094e270fb54
g00310497 <victor.gao@huawei.com>
2016-09-13 22:10:31 +0800

    change:implement the vnfm exception .
    Change-Id: I17d5aeb417a6862b3936f444dbd486cb696bad45
    Signed-off-by: g00310497 <victor.gao@huawei.com>

--- drivers/vnfm/svnfm/huawei/vnfmadapter/VnfmadapterService/service/src/main/java/org/openo/nfvo/vnfmadapter/common/VnfmException.java
+++ drivers/vnfm/svnfm/huawei/vnfmadapter/VnfmadapterService/service/src/main/java/org/openo/nfvo/vnfmadapter/common/VnfmException.java
@@ -1,5 +1,5 @@
-/*******************************************************************************
- * Copyright 2016, Huawei Technologies Co., Ltd.
+/*
+ * Copyright 2016 Huawei Technologies Co., Ltd.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -12,13 +12,35 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *******************************************************************************/
+ */
+
 package org.openo.nfvo.vnfmadapter.common;
 
+/**
+ * VNFM exception.
+ * .</br>
+ * 
+ * @author
+ * @version     NFVO 0.5  Sep 10, 2016
+ */
 public class VnfmException extends Exception {
 
-	public VnfmException(String string) {
-		// TODO Auto-generated constructor stub
-	}
+    private static final long serialVersionUID = 1L;
+
+    public VnfmException() {
+        super();
+    }
+
+    public VnfmException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public VnfmException(String message) {
+        super(message);
+    }
+
+    public VnfmException(Throwable cause) {
+        super(cause);
+    }
 
 }
