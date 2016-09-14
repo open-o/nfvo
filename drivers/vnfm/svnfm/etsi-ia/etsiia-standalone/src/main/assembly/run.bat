e501dddc9c1e52ac89deae4d81ab02e3b1aefa35
wangchengli <wangchengli@chinamobile.com>
2016-09-14 17:25:30 +0800

    fix License Headers
    Change-Id: Iea03144ccf434886f237515edc1ef5159f4bd78f
    Signed-off-by: wangchengli <wangchengli@chinamobile.com>

--- drivers/vnfm/svnfm/etsi-ia/etsiia-standalone/src/main/assembly/run.bat
+++ drivers/vnfm/svnfm/etsi-ia/etsiia-standalone/src/main/assembly/run.bat
@@ -1,5 +1,5 @@
 @REM
-@REM Copyright (C) 2015 CMCC, Inc. and others. All rights reserved. (CMCC)
+@REM Copyright 2016, CMCC Technologies Co., Ltd.
 @REM
 @REM Licensed under the Apache License, Version 2.0 (the "License");
 @REM you may not use this file except in compliance with the License.
