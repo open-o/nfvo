c05a058356dc3dfbe2fd11d8f3616401e64a58ca
10121394 <sun.jianli@zte.com.cn>
2016-09-23 11:04:40 +0800

    test commit
    Change-Id: I8389db64a6f14952e9dc3c5310477a57bb5cc733
    Signed-off-by: 10121394 <sun.jianli@zte.com.cn>

--- drivers/vnfm/svnfm/zte/vmanager/manage.py
+++ drivers/vnfm/svnfm/zte/vmanager/manage.py
@@ -17,6 +17,7 @@ import sys
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "driver.settings")
 
+# load initial configuration
 if __name__ == "__main__":
     from django.core.management import execute_from_command_line
     execute_from_command_line(sys.argv)
