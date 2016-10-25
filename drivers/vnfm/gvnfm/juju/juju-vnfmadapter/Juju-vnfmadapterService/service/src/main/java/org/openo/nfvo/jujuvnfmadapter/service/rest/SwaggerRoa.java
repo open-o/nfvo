bb4bed1478fac89ce125f0635908c7b079f71882
d00336982 <dongyanyi@huawei.com>
2016-10-25 16:34:30 +0800

    IssueId:NFVO-42
    Change-Id: Ifd2bbe363a89c3e0106f0fc097415d176f758297
    Signed-off-by: d00336982 <dongyanyi@huawei.com>

--- drivers/vnfm/gvnfm/juju/juju-vnfmadapter/Juju-vnfmadapterService/service/src/main/java/org/openo/nfvo/jujuvnfmadapter/service/rest/SwaggerRoa.java
+++ drivers/vnfm/gvnfm/juju/juju-vnfmadapter/Juju-vnfmadapterService/service/src/main/java/org/openo/nfvo/jujuvnfmadapter/service/rest/SwaggerRoa.java
@@ -32,7 +32,7 @@ import org.apache.commons.io.IOUtils;
  * @author
  * @version NFVO 0.5 Oct 24, 2016
  */
-@Path("/openoapi/jujuvnfm/v1")
+@Path("/v1")
 @Produces({MediaType.APPLICATION_JSON})
 public class SwaggerRoa {
     /**
