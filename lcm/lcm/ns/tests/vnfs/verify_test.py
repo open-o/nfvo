9359ea7800a5a011658b09f723a742df7168e710
ying.yunlong <ying.yunlong@zte.com.cn>
2017-05-08 10:14:03 +0800

    Add call func test logic of verify vnfs
    Change-Id: Ia9a4bcd239b529421da8b7a06720577e07c54f5c
    Issue-Id: NFVO-173
    Signed-off-by: ying.yunlong <ying.yunlong@zte.com.cn>

--- lcm/lcm/ns/tests/vnfs/verify_test.py
+++ lcm/lcm/ns/tests/vnfs/verify_test.py
@@ -18,7 +18,7 @@ from rest_framework import status
 class TestGetVnfViews(TestCase):
     def setUp(self):
         self.client = Client()
-        self.data = {"csarId":"1223334"}
+        self.data = {"PackageID":"1223334"}
         pass
 
     def tearDown(self):
